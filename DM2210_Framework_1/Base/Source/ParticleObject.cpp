6867cde70a9918abbd534c8403eee581609be9c8
Lorenzo Yanga <lorenzoyanga@gmail.com>
2019-07-18 15:11:19 +0800

    slight edit to particleobject code

--- DM2210_Framework_1/Base/Source/ParticleObject.cpp
+++ DM2210_Framework_1/Base/Source/ParticleObject.cpp
@@ -2,6 +2,7 @@
 
 ParticleObject::ParticleObject(GameObject::GAMEOBJECT_TYPE typeValue = GameObject::GO_NIL) // defaults to a nil
 	: GameObject(typeValue),
+	f_lifespan(0.f),
 	rotation(0.f),
 	rotationspeed(0.f)
 {
