ef4dc22f9f7b8170615d3d50d30c454f372e27bf
Lorenzo Yanga <lorenzoyanga@gmail.com>
2019-07-30 12:22:15 +0800

    variable cycle speed + bug fix

--- DM2210_Framework_1/Base/Source/DayNightCycler.cpp
+++ DM2210_Framework_1/Base/Source/DayNightCycler.cpp
@@ -5,7 +5,8 @@ DayNightCycler::DayNightCycler()
 	v_sunPos(0, 0, 0),
 	v_moonPos(0, 0, 0),
 	f_cyclePoint(0.f),
-	f_amplitude(0.f)
+	f_amplitude(0.f),
+	f_cycleSpeed(1.f)
 {
 }
 
@@ -15,13 +16,16 @@ DayNightCycler::~DayNightCycler()
 
 void DayNightCycler::Update(double dt)
 {
-	f_cyclePoint += (float)dt;
-	if (f_cyclePoint > Math::PI)
+	f_cyclePoint += (float)dt * 0.05 * f_cycleSpeed;
+	if (f_cyclePoint > Math::TWO_PI)
 	{
-		f_cyclePoint -= Math::PI;
+		f_cyclePoint -= Math::TWO_PI;
 	}
 
-	v_sunPos.Set(sin(f_cyclePoint), cos(f_cyclePoint), 0);
-	v_sunPos * f_amplitude;
+	v_sunPos.Set(sin(f_cyclePoint) * f_amplitude, cos(f_cyclePoint) * f_amplitude, 0);
 	v_moonPos = -v_sunPos;
+
+	f_hour = (int)((f_cyclePoint / Math::TWO_PI) * 24);
+	
+	//cout << v_sunPos << '\n';
 }
--- DM2210_Framework_1/Base/Source/DayNightCycler.h
+++ DM2210_Framework_1/Base/Source/DayNightCycler.h
@@ -2,14 +2,18 @@
 #include "MyMath.h"
 #include "Vector3.h"
 
+#include <iostream>
+using namespace std;
 class DayNightCycler
 {
 public:
 	// in a complete oscillation (from 1 to pi)
 	float f_cyclePoint;
-
+	float f_cycleSpeed;
 	// radius
 	float f_amplitude;
+
+	float f_hour;
 
 	// for simplicity, sun and moon positions are at complete opposites.
 	Vector3 v_sunPos;
